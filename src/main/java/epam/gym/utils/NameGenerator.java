096285e12ce0c63a331e3bfce7e68fb21b12f170
VitaliiKalinchyk <karlkory@gmail.com>
2024-08-31 21:29:18 +0300

    Added logic for NameGenerator

--- src/main/java/epam/gym/utils/NameGenerator.java
+++ src/main/java/epam/gym/utils/NameGenerator.java
@@ -1,4 +1,15 @@
 package epam.gym.utils;
 
+import epam.gym.entity.Trainee;
+import epam.gym.entity.Trainer;
+import epam.gym.entity.User;
+
+import java.util.List;
+
 public interface NameGenerator {
+
+    String generateUsername(User user);
+
+    String generateUsername(User user, List<Trainee> trainees, List<Trainer> trainers);
+
 }
--- src/main/java/epam/gym/utils/impl/NameGeneratorImpl.java
+++ src/main/java/epam/gym/utils/impl/NameGeneratorImpl.java
@@ -1,4 +1,30 @@
 package epam.gym.utils.impl;
 
-public class NameGeneratorImpl {
+import epam.gym.entity.Trainee;
+import epam.gym.entity.Trainer;
+import epam.gym.entity.User;
+import epam.gym.utils.NameGenerator;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component
+public class NameGeneratorImpl implements NameGenerator {
+    @Override
+    public String generateUsername(User user) {
+        return user.getFirstName() + "." + user.getLastName();
+    }
+
+    @Override
+    public String generateUsername(User user, List<Trainee> trainees, List<Trainer> trainers) {
+        String baseUsername = generateUsername(user);
+        int maxIndex = Stream.concat(trainees.stream(), trainers.stream())
+                             .map(User::getUsername)
+                             .map(userName -> userName.replaceAll("\\D+?(\\d)", "$1"))
+                             .mapToInt(Integer::parseInt)
+                             .max()
+                             .orElse(0);
+        return baseUsername + (maxIndex + 1);
+    }
 }
