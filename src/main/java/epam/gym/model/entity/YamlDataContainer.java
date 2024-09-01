4f56c3e42d2c56ee7e66cd27bad5ebaca4dfcb8a
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-01 15:13:28 +0300

    better YamlDataContainer

--- src/main/java/epam/gym/model/entity/YamlDataContainer.java
+++ src/main/java/epam/gym/model/entity/YamlDataContainer.java
@@ -7,12 +7,13 @@ import epam.gym.model.model.Training;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class YamlDataContainer {
-    private List<Trainee> trainees;
-    private List<Trainer> trainers;
-    private List<Training> trainings;
+    private final List<Trainee> trainees = new ArrayList<>();
+    private final List<Trainer> trainers = new ArrayList<>();
+    private final List<Training> trainings = new ArrayList<>();
 }
