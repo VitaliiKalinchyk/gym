163483635c1a1d3312b6276c34b7c38f3e9b4962
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-01 10:12:04 +0300

    added setters to Storage interface

--- src/main/java/epam/gym/storage/Storage.java
+++ src/main/java/epam/gym/storage/Storage.java
@@ -1,13 +1,18 @@
 package epam.gym.storage;
 
-import epam.gym.entity.Trainee;
-import epam.gym.entity.Trainer;
-import epam.gym.entity.Training;
+import epam.gym.model.model.Trainee;
+import epam.gym.model.model.Trainer;
+import epam.gym.model.model.Training;
 
 import java.util.Map;
 
 public interface Storage {
     Map<Integer, Trainee> getTrainees();
+    void setTrainees(Map<Integer, Trainee> trainees);
+
     Map<Integer, Trainer> getTrainers();
+    void setTrainers(Map<Integer, Trainer> trainers);
+
     Map<Integer, Training> getTrainings();
+    void setTrainings(Map<Integer, Training> trainings);
 }
