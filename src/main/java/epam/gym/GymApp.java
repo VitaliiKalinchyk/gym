6bdff443f16a0f6597db381cf5fe2824569043f2
VitaliiKalinchyk <karlkory@gmail.com>
2024-09-06 08:54:05 +0300

    added jacoco

--- src/main/java/epam/gym/GymApp.java
+++ src/main/java/epam/gym/GymApp.java
@@ -1,21 +1,18 @@
 package epam.gym;
 
 import epam.gym.config.GymAppConfig;
-import epam.gym.entity.Trainee;
+import epam.gym.facade.GymFacade;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.Map;
 
 public class GymApp {
     public static void main( String[] args ) {
         ApplicationContext context = new AnnotationConfigApplicationContext(GymAppConfig.class);
 
-        Map<Integer, Trainee> trainees = context.getBean("trainees", Map.class);
+        GymFacade facade = context.getBean(GymFacade.class);
 
-        trainees.put(1, new Trainee());
-
-        System.out.println(trainees.get(1));
+        System.out.println(facade.traineeService().getTrainees());
     }
 }
